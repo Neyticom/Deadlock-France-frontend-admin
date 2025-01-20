0ee5dcc8a07f8001f6dd659d4412df7ccb3c3c4a
NPanisson <panisson.nils@gmail.com>
2025-01-20 17:56:49 +0100

    login page ok responsive ok

--- src/views/LoginPage.tsx
+++ src/views/LoginPage.tsx
@@ -1,6 +1,7 @@
 import type React from "react";
 import {useState} from "react";
 import { useNavigate } from "react-router-dom";
+import './LoginPage.scss';
 
 function LoginPage(){
 
@@ -19,44 +20,48 @@ function LoginPage(){
   };
 
     return (
-      <>
+      <div className="login-page__container">
       <header>
         <h1 className="login-page__title">Pannel Admin</h1>
       </header>
       <main className="login-page__content">
         <div className="login-page__logo-container">
           <img
-            src="/home/student/Deadlock-France-frontend-admin/src/assets/icons/logo.png"
+            src="src/assets/icons/logo.png"
             alt="logo deadlock france"
             className="login-page__logo-container_image"
           />
           <span className="login-page__logo-container_title">
-            Deadlock France
+            <span>Deadlock</span>
+            <span>France</span> 
           </span>
         </div>
-        <form onSubmit={handleSubmit}>
-          <div>
+        <form className="login-page__content__form" onSubmit={handleSubmit}>
+          <div className="login-page__content__inputs">
+            <label htmlFor="username">Identifiant</label>
             <input
               type="text"
               name="username"
               id="username"
-              placeholder="Nom d'utilisateur"
+              // placeholder="Nom d'utilisateur"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
               required
             />
           </div>
-          <div>
+          <div className="login-page__content__inputs">
+          <label htmlFor="password">Mot de passe</label>
             <input
               type="password"
               name="password"
               id="password"
-              placeholder="Mot de passe"
+              // placeholder="Mot de passe"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
             />
           </div>
+          <a href="forgotten-password">Mot de passe oublié ?</a>
           <div>
             <button type="submit" className="login-page__submit-button">
               Se connecter
@@ -64,7 +69,8 @@ function LoginPage(){
           </div>
         </form>
       </main>
-    </>
+      <footer><a href="/"> {'\u2039'}- Retourner à Deadlock France</a></footer>
+    </div>
     );
 };
 
