733b90eb11171608186482ae2f8f6564e980ac51
Romain B. (Neyticom) <romainb.boitepro@gmail.com>
2025-02-02 21:55:41 +0100

    chore: Reorganized repository tree structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,113 @@
-import { Route, Routes } from "react-router-dom";
-import './App.scss';
-import LoginPage from '/home/student/Deadlock-France-frontend-admin/src/views/LoginPage.tsx';
+import { useEffect, useState } from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import React from 'react';
+import Loader from './common/Loader';
+import PageTitle from './components/PageTitle';
+import LoginPage from './pages/Authentication/LoginPage';
+import SignUp from './pages/Authentication/SignUp';
+import Chart from './pages/Chart';
+import AdminDashboard from './pages/Dashboard/AdminDashboard';
+import Settings from './pages/Settings';
+import Alerts from './pages/UiElements/Alerts';
+import Buttons from './pages/UiElements/Buttons';
+import DefaultLayout from './layout/DefaultLayout';
+import UserManagement from './pages/UserManagement';
+import PatchnotesManagement from './pages/PatchnotesManagement';
 
+function App() {
+  const [loading, setLoading] = useState<boolean>(true);
+  const { pathname } = useLocation();
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
-const App = () => {
-  return (
-    <div className="admin-app-container">
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
+  return loading ? (
+    <Loader />
+  ) : (
+    <DefaultLayout>
       <Routes>
-        {/* Login Page */}
-        <Route path="/admin" element={<LoginPage />} />
 
-        {/* Main Admin Dashboard */}
-        {/* <Route path="/admin/dashboard" element={<AdminDashboard />} /> */}
+      <Route
+          path="/auth/signin"
+          element={
+            <>
+              <PageTitle title="Signin | Deadlock France Admin Dashboard" />
+              <LoginPage />
+            </>
+          }/>
+        <Route
+          path="/auth/signup"
+          element={
+            <>
+              <PageTitle title="Signup | Deadlock France Admin Dashboard" />
+              <SignUp />
+            </>
+          }/>
+
+        <Route
+          path='/admin'
+          element={
+            <>
+              <PageTitle title="Dashboard | Deadlock France Admin Dashboard" />
+              <AdminDashboard />
+            </>
+          }/>
+        <Route
+          path="/admin/userManagement"
+          element={
+            <>
+              <PageTitle title="Gestion des utilisateurs | Deadlock France User Management" />
+              <UserManagement />
+            </>
+          }/>
+        <Route
+          path="/admin/patchnotesManager"
+          element={
+            <>
+              <PageTitle title="Gestion des Patchnotes | Deadlock France Patchnote Management" />
+              <PatchnotesManagement />
+            </>
+          }/>
+        <Route
+          path="/settings"
+          element={
+            <>
+              <PageTitle title="Settings | Deadlock France Admin Settings" />
+              <Settings />
+            </>
+          }/>
+        <Route
+          path="/chart"
+          element={
+            <>
+              <PageTitle title="Basic Chart | Deadlock France Admin Dashboard" />
+              <Chart />
+            </>
+          }/>
+        <Route
+          path="/ui/alerts"
+          element={
+            <>
+              <PageTitle title="Alerts | Deadlock France Admin Dashboard" />
+              <Alerts />
+            </>
+          }/>
+        <Route
+          path="/ui/buttons"
+          element={
+            <>
+              <PageTitle title="Buttons | Deadlock France Admin Dashboard" />
+              <Buttons />
+            </>
+          }/>
       </Routes>
-    </div>
+    </DefaultLayout>
   );
-};
+}
 
 export default App;
